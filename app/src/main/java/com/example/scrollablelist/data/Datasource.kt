14a2a10af0bdf97a3f508727ca1f05d975582ca1
uzyko <uzykoleksandr@gmail.com>
2026-04-19 13:54:04 +0300

    feat: add class for load Affirmations

---
+++ app/src/main/java/com/example/scrollablelist/data/Datasource.kt
@@ -0,0 +1,15 @@
+package com.example.scrollablelist.data
+
+import com.example.scrollablelist.R
+import com.example.scrollablelist.model.Affirmation
+
+class Datasource {
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2),
+            Affirmation(R.string.affirmation3, R.drawable.image3),
+            Affirmation(R.string.affirmation4, R.drawable.image4)
+        )
+    }
+}
